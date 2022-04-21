bb02fa4ac15c6b16ddc534dc341aac5da709feda
Zhentao-Lin <62634190+Zhentao-Lin@users.noreply.github.com>
2022-04-21 16:57:19 +0800

    Update Upload the bin.bat

--- Code/Windows/Upload the bin.bat
+++ Code/Windows/Upload the bin.bat
@@ -4,6 +4,10 @@
 
 echo.
 
+esptool.exe --port COM4 erase_flash
+
+echo.
+
 esptool.exe --chip esp32 --port COM4 --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ../firmware/ota_data_initial.bin 0x1000 ../firmware/bootloader/bootloader.bin 0x10000 ../firmware/esp32_idf_dog.bin 0x8000 ../firmware/partition_table/partition-table.bin
 
 echo.
