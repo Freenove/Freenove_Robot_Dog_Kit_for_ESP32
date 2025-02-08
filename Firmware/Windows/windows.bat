e94d6cc0aed4a33d69a93f862c9a8f37352565ee
Jeffers96 <luke.jeffers5@outlook.com>
2025-02-08 21:01:36 +0800

    Update windows.bat

--- Firmware/Windows/windows.bat
+++ Firmware/Windows/windows.bat
@@ -4,11 +4,11 @@
 
 echo.
 
-esptool.exe --port COM4 erase_flash
+esptool.exe --port COM3 erase_flash
 
 echo.
 
-esptool.exe --chip esp32 --port COM4 --baud 1000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ../firmware/ota_data_initial.bin 0x1000 ../firmware/bootloader/bootloader.bin 0x10000 ../firmware/esp32_idf_dog.bin 0x8000 ../firmware/partition_table/partition-table.bin
+esptool.exe --chip esp32 --port COM3 --baud 1000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ../firmware/ota_data_initial.bin 0x1000 ../firmware/bootloader/bootloader.bin 0x10000 ../firmware/esp32_idf_dog.bin 0x8000 ../firmware/partition_table/partition-table.bin
 
 echo.
 
